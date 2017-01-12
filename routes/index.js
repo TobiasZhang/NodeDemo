6f0e26b8a2cef3244d316d996b945f7890c3c9af
TT <tt>
2017-01-12 16:33:05 +0800

    第二次提交

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express',movies:[{_id:1,title:'大片1',img:'/images/test01.jpg'},{_id:333,title:'大片2',img:'/images/test02.jpg'},] });
 });
 
 module.exports = router;
